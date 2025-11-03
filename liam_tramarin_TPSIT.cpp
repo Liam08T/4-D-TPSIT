f80b220e909967e3db663f24011d7e06b60a3155
Liam08T <tramarinliam@gmail.com>
2025-11-03 10:51:00 +0100

    Add files via upload

---
+++ liam_tramarin_TPSIT.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+using namespace std;
+
+class Task {
+    private:
+        
+    
+    public:
+
+};
+
+class Worker {
+    private:
+    
+    public:
+
+};
+
+class Master {
+    private:
+    
+    public:
+};
+
+
+int main() {
+        
+    
+
+	return 0;
+}
